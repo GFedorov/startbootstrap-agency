d127362bd4c606088eeb82f91514f81cff2f6cde
Gri_Fedorov <fedorov.grigory@gmail.com>
2019-06-02 10:48:45 +0300

    header-shirnk nav bar removed(rgba) rgba added to header and text

--- js/agency.js
+++ js/agency.js
@@ -27,7 +27,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  /*var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -38,5 +38,5 @@
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-
+*/
 })(jQuery); // End of use strict
