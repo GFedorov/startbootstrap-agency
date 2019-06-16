e64a8f814e543a33c54ec32b0d8b01169f97732d
Gri_Fedorov <fedorov.grigory@gmail.com>
2019-06-16 10:40:54 +0300

    hadlebars simple page added

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+var exphbs  = require('express-handlebars');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+ 
+
+app.get('/contacts', (req, res) => res.send('Contatcs page'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+ 
+ 
